934444f15514c42b2a440f0e8d5a6b84d9cd3597
Aidan Fay <aidanfay@stanford.edu>
2018-07-20 12:02:45 -0400

    Clean rtde_urx_mixed_control.

--- rtde_urx_mixed_control/mixed_control/mixed_control_send_traj.py
+++ rtde_urx_mixed_control/mixed_control/mixed_control_send_traj.py
@@ -8,7 +8,7 @@
 #    * Redistributions in binary form must reproduce the above copyright
 #      notice, this list of conditions and the following disclaimer in the
 #      documentation and/or other materials provided with the distribution.
-#    * Neither the name of the Universal Robots A/S nor the names of its
+#    * Neither the namme of the Universal Robots A/S nor the names of its
 #      contributors may be used to endorse or promote products derived
 #      from this software without specific prior written permission.
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
@@ -25,14 +25,14 @@
 import sys
 sys.path.append('..')
 import logging
-
 import rtde.rtde as rtde
 import rtde.rtde_config as rtde_config
-
+import urx.urscript
+import urx.robot
 
 #logging.basicConfig(level=logging.INFO)
 
-ROBOT_HOST = '192.168.12.248'
+ROBOT_HOST = '192.168.12.50'
 ROBOT_PORT = 30004
 config_filename = 'control_loop_configuration.xml'
 
@@ -56,10 +56,6 @@ con.send_output_setup(state_names, state_types)
 setp = con.send_input_setup(setp_names, setp_types)
 watchdog = con.send_input_setup(watchdog_names, watchdog_types)
 
-# Setpoints to move the robot to
-setp1 = [-0.12, -0.43, 0.14, 0, 3.11, 0.04]
-setp2 = [-0.12, -0.51, 0.21, 0, 3.11, 0.04]
-
 setp.input_double_register_0 = 0
 setp.input_double_register_1 = 0
 setp.input_double_register_2 = 0
@@ -69,6 +65,26 @@ setp.input_double_register_5 = 0
   
 # The function "rtde_set_watchdog" in the "rtde_control_loop.urp" creates a 1 Hz watchdog
 watchdog.input_int_register_0 = 0
+
+urxRob = urx.Robot(ROBOT_HOST)
+
+traj = []
+
+
+def add_multiple_to_traj(new_setps):
+    for new_setp in new_setps:
+        add_to_traj(new_setp)
+
+
+def add_to_traj(new_setp):
+    list = [0,0,0,0,0,0]
+    list[0] = new_setp[0]
+    list[1] = new_setp[1]
+    list[2] = new_setp[2]
+    list[3] = new_setp[3]
+    list[4] = new_setp[4]
+    list[5] = new_setp[5]
+    traj.append(list)
 
 def setp_to_list(setp):
     list = []
@@ -81,9 +97,28 @@ def list_to_setp(setp, list):
         setp.__dict__["input_double_register_%i" % i] = list[i]
     return setp
 
+def start_RTDE_servo_listener():
+    file = open("rtde_control_servo.script","r")
+    prog = urx.urscript.URScript()
+    for line in file:
+        prog.add_line_to_program(line)
+    urxRob.send_program(prog.__call__())
+
+def generate_traj():
+    list = []
+    for j in range(0, 800):
+        z = 0.000002 * j * j
+        y = -1.80 + (0.001 * j)
+        list.append([z, y, 0, 0, 0, 0])
+    return list
+
+add_multiple_to_traj(generate_traj())
+
 #start data synchronization
 if not con.send_start():
     sys.exit()
+
+start_RTDE_servo_listener()
 
 # control loop
 while keep_running:
@@ -91,14 +126,17 @@ while keep_running:
     state = con.receive()
     
     if state is None:
-        break;
-    
+        break
+
     # do something...
     if state.output_int_register_0 != 0:
-        new_setp = setp1 if setp_to_list(setp) == setp2 else setp2
-        list_to_setp(setp, new_setp)
-        # send new setpoint        
-        con.send(setp)
+
+        if len(traj) > 0:
+            newSetp = traj.pop(0)
+            setp = list_to_setp(setp, newSetp)
+
+            # send new setpoint
+            con.send(setp)
 
     # kick watchdog
     con.send(watchdog)
